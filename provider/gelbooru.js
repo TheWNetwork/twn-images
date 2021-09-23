d1d2b7e8f1bf21ccc7cdc6a2a9b9a09c9ca37578
Kevin Cala Sánchez <kevin@kcsystem.es>
2021-09-23 17:16:45 +0200

    Gelbooru Fix

--- provider/gelbooru.js
+++ provider/gelbooru.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 module.exports.run = async (provider) => {
     return axios(`${provider.destination}?page=dapi&s=post&json=1&q=index&random=1&limit=1&api_key=${provider.api_key}&user_id=${provider.api_user}&tags=sort:random ${provider.endpoint}`)
         .then(function (result) {
-            return result.data.file_url
+            return result.data[0].file_url
         });
 }
 
