a83c16e9896fa08af3ce7bb3f9945be43046d77a
Kevin Cala Sánchez <kevin@kcsystem.es>
2021-09-23 18:15:08 +0200

    PingPong

---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+module.exports.run = async (botconfig, dbclient, bot, message, args) => {
+    bot.sendMessage(message.chat.id, "Pong. \n\n" +
+        "You can check our services on @TheWNetwork.");
+};
+
+module.exports.help = {
+    name: "start"
+};
