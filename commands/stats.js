80f63ba9f3096f2cdb83fe145f299b5ee629cc3d
Kevin Cala Sánchez <kevin@kcsystem.es>
2021-09-25 02:02:59 +0200

    Stats here

--- commands/stats.js
+++ commands/stats.js
@@ -1,8 +1,10 @@
 module.exports.run = async (botconfig, pool, bot, message, args) => {
     let qry = 'SELECT sum(times) counter FROM groups g;';
+    let qryhere = `SELECT sum(times) counter FROM groups g WHERE id_telegram = ${message.chat.id};`;
     let result = await pool.query(qry);
+    let resulthere = await pool.query(qryhere);
 
-    bot.sendMessage(message.chat.id, `Requests served: ${result[0].counter}\n\nYou can check our services on @TheWNetwork.`);
+    bot.sendMessage(message.chat.id, `Requests served total: ${result[0].counter}\nRequests served on this chat:${resulthere[0].counter}\n\nYou can check our services on @TheWNetwork.`);
 };
 
 module.exports.help = {
