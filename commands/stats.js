9f4918eecb830dbc4d8d1d0cec045e64fe12218d
Kevin Cala Sánchez <kevin@kcsystem.es>
2022-03-18 18:51:49 +0100

    Now show total groups

--- commands/stats.js
+++ commands/stats.js
@@ -1,10 +1,14 @@
 module.exports.run = async (botconfig, pool, bot, message, args) => {
-    let qry = 'SELECT sum(times) counter FROM groups g;';
+    let qry = 'SELECT sum(times) counter, count(1) as groups FROM groups g;';
     let qryhere = `SELECT sum(times) counter FROM groups g WHERE id_telegram = ${message.chat.id};`;
     let result = await pool.query(qry);
     let resulthere = await pool.query(qryhere);
 
-    bot.sendMessage(message.chat.id, `Requests served total: ${result[0].counter}\nRequests served on this chat:${resulthere[0].counter}\n\nYou can check our services on @TheWNetwork.`);
+    bot.sendMessage(message.chat.id, `Total Groups:${$result[0].groups}
+Requests served total: ${result[0].counter}
+Requests served on this chat:${resulthere[0].counter}
+
+You can check our services on @TheWNetwork.`);
 };
 
 module.exports.help = {
